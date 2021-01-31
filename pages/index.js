475ef72cc372da1020ed04327e1f8359aee57e77
João Bispo <vitor.vi10@hotmail.com>
2021-01-31 19:45:35 -0300

    style: added widget component

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,50 @@
 import styled from 'styled-components'
+import db from '../db.json';
+import Widget from '../src/components/Widget';
 
 const Title = styled.h1`
   font-size: 50px;
   color: ${({ theme }) => theme.colors.primary};
 `
+const BackgroundImage = styled.div`
+  background-image: url(${db.bg});
+  background-size: cover;
+  background-position: center;
+  flex: 1;  
+`
+
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <BackgroundImage>
+      <QuizContainer>
+        <Widget>
+          <Widget.Content>
+            <Widget.Header>
+              <h1>Filosofia Antiga</h1>
+            </Widget.Header>
+            <p>Lorem ipsum dolor sit amet, consectetur adip</p>
+          </Widget.Content>
+        </Widget>
+        <Widget>
+          <Widget.Content>
+            <Widget.Header>
+              <h1>Filosofia Antiga</h1>
+            </Widget.Header>
+            <p>Lorem ipsum dolor sit amet, consectetur adip</p>
+          </Widget.Content>
+        </Widget>
+      </QuizContainer>
+    </BackgroundImage>
+  )
 }
